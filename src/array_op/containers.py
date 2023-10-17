b0c3325fcdee6d95b987004600f79cb5bed5f1c8
Cheng Qiu <cheng.qiu@vanderbilt.edu>
2023-10-17 13:26:20 -0500

    implemented queue

--- src/array_op/containers.py
+++ src/array_op/containers.py
@@ -1,3 +1,6 @@
+"""
+@brief      A simple node for the LinkedList
+"""
 class LinkedNode():
     def __init__(self, value=None):
         self.value = value
@@ -44,6 +47,19 @@ class LinkedList():
         self.size = self.size + 1
 
     """
+    @brief Add the node to the start of the linked list
+    """
+    def addNodeHead(self, value):
+        if(self.isEmpty()):
+            self.head = LinkedNode(value)
+            self.tail = self.head
+        else:
+            newNode = LinkedNode(value)
+            LinkedNode.linkNode(newNode, self.head)
+            self.head = newNode
+        self.size = self.size + 1
+
+    """
     @brief overloaded add node in between two nodes
     """
     def addNode(self, value, prev, next):
@@ -74,12 +90,15 @@ class LinkedList():
     @param  n   The index of the node to be removed
     """
     def removeNodeIndex(self, n):
+        if(self.isEmpty()):
+            raise Exception("Attempting to remove from empty list")
         if(n == 0):
             self.head = self.head.next
         elif(n == self.size-1):
             self.tail = self.tail.prev
         else:
             LinkedNode.unlinkNode(self.getNodeIndex(n))
+        self.size = self.size - 1
 
 
     def isEmpty(self):
@@ -92,7 +111,7 @@ class LinkedList():
         returnStr = ""
         current = self.head
         for i in range(self.size):
-            returnStr += current.value + " "
+            returnStr += str(current.value) + " "
             current = current.next
         return returnStr
 
@@ -119,6 +138,60 @@ class Stack():
             raise Exception("Attempting to peek into an empty stack")
         return self.__array[self.__index-1]
 
-# class queue():
-#     def __init__(self):
-#         self.array
+"""
+@brief  A linked list implementation of Queue structure
+"""
+class Queue():
+    def __init__(self):
+        self.list = LinkedList()
+
+    """
+    @brief          Add element to the front of the queue
+    @param  value   The value to be inserted
+    """
+    def addFront(self, value):
+        self.list.addNodeHead(value)
+
+    """
+    @brief          Add element to the back of the queue
+    @param  value   The value to be inserted
+    """
+    def addBack(self, value):
+        self.list.addNodeTail(value)
+
+
+    """
+    @brief          Remove element from the front of the queue
+    """
+    def popFront(self):
+        self.list.removeNodeIndex(0)
+
+    """
+    @brief          Remove element from the back of the queue
+    """
+    def popBack(self):
+        self.list.removeNodeIndex(self.list.size-1)
+
+    """
+    @brief          Get the element at the front of the queue
+    @return         The value at the front
+    """
+    def peekFront(self):
+        return self.list.getNodeIndex(0).getValue()
+
+    """
+    @brief          Get the element at the back of the queue
+    @return         The value at the back
+    """
+    def peekBack(self):
+        return self.list.getNodeIndex(self.list.size - 1).getValue()
+
+    """
+    @brief          Overloaded __str__ method
+    @return         The elements in the linked list
+    """
+    def __str__(self):
+        returnStr = self.list.__str__()
+        return returnStr
+
+
